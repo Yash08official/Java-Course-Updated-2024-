268b531b02ae34aff183b6e989855db4865f3996
Yash08official <yashwasankar008@gmail.com>
2024-07-29 19:47:08 +0530

    The New Line -Java

---
+++ A_Easy_questions/Geeks.java
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Geeks{
+    
+  // Function to print each word in single line
+  public static void main(String[] args){
+      
+     System.out.println("Geeks");
+     System.out.println("for");
+     System.out.println("Geeks");
+      
+  }
+  
+}
+
+// For Input: 
+// 1
+
+// Your Output: 
+// Geeks
+// for
+// Geeks
+
+// Expected Output: 
+// Geeks
+// for
+// Geeks
